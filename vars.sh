5385c231ca86cf0269e1879c2ffbeb4a8b9ce5ec
pms <pms@ifisc.uib-csic.es>
2013-10-21 19:33:43 +0200

    updated with cpp netlib

--- vars.sh
+++ vars.sh
@@ -1,20 +1,32 @@
+# PLEASE INPUT PATHS TO THE SPECIFIED LIBRARIES BELOW
+
 # path to include directory of Boost library (http://www.boost.org/)
 # Boost version has to be 1.44 or higher
 # e.g. BOOST_SRC=/usr/local/include/
 export BOOST_SRC=/usr/local/include/
 
-# path to directory with binaries of Boost library
+# path to the directory with binaries of Boost library
 # e.g. BOOST_BIN=/usr/local/lib
 export BOOST_BIN=/usr/local/lib
 
 # path to include directory of JsonCpp (http://jsoncpp.sourceforge.net/)
 # JsonCpp version has to 0.5.0 or higher
 # e.g. JSON_SRC=~/jsoncpp-src-0.5.0/include
-export JSON_SRC=fill_here
+export JSON_SRC=FILL_HERE
 
 # path to the compiled JsonCpp library
 # if no prefix was given during the installation then
 # the library is in ~/jsoncpp-src-0.5.0/libs/PLACEHOLDER/libjson_PLACEHOLDER_libmt.{a,so}
 # where PLACEHOLDER depends on the compiler used and will vary
 # e.g. JSON_LIBMT=~/jsoncpp-src-0.5.0/libs/linux-gcc-4.2.4/libjson_linux-gcc-4.2.4_libmt
-export JSON_LIBMT=fill_here
+export JSON_LIBMT=FILL_HERE
+
+# path to include directory of cpp-netlib library (http://cpp-netlib.org)
+# e.g. CPPNET_SRC=~/cpp-netlib-0.10.1/build/libs/network/src
+export CPPNET_SRC=FILL_HERE
+
+# path to the directory with binaries of cpp-netlib library
+# e.g. CPPNET_BIN=~/cpp-netlib-0.10.1/build/libs/network/src
+export CPPNET_BIN=FILL_HERE
+
+
